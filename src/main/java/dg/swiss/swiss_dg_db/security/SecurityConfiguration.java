648159331ba58d0e95b2700b9bd502eeb57d1ef3
Christopher Walker <512444+cjw4@users.noreply.github.com>
2025-11-15 08:13:06 -0500

    start to security, return to later

---
+++ src/main/java/dg/swiss/swiss_dg_db/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package dg.swiss.swiss_dg_db.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User
+                .withUsername("swisstour")
+                .password(passwordEncoder.encode("swisstour"))
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/api/events").authenticated();
+                    authorize.anyRequest().permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
