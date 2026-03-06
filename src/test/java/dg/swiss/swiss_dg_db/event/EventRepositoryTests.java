03bbb73cf20105d2f55b0ffdd45ad65782f04593
Christopher <512444+cjw4@users.noreply.github.com>
2026-03-06 11:07:08 +0100

    fix test

--- src/test/java/dg/swiss/swiss_dg_db/event/EventRepositoryTests.java
+++ src/test/java/dg/swiss/swiss_dg_db/event/EventRepositoryTests.java
@@ -52,7 +52,7 @@ public class EventRepositoryTests {
         boolean exists = eventRepository.existsByEventId(70000L);
 
         // Assert
-        Assertions.assertThat(exists).isTrue();
+        Assertions.assertThat(exists).isFalse();
     }
 
 }
